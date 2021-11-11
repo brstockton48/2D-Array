6da52d07cf77e2cc2be6e3d3e9d5599b01a67cb7
Billy Stockton <90619690+brstockton48@users.noreply.github.com>
2021-11-10 21:07:25 -0500

    Add files via upload

---
+++ 2 Dim Arrays/Matrix.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+/// <summary>
+/// Billy Stockton
+/// 2DArray Exercise
+/// Due 6/10/2021
+/// This Program prints, transposes and traces matrices
+/// </summary>
+
+namespace Stockton2DArray
+{
+    
+    public class Matrix
+    {
+        /// <summary>
+        /// Class Constants/Arrays/Attributes
+        /// </summary>
+        public int currentRow = 0;
+        public int maxRow = 4;
+        public int startCol = 0;
+        public int maxCol = 4;
+        public int traceResult = 0;
+        public int[,] twoDArray = new int[4,4];
+        public int[,] transArray = new int[4, 4];
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="CurrentRow"></param>
+        /// <param name="MaxRow"></param>
+        /// <param name="StartCol"></param>
+        /// <param name="MaxCol"></param>
+        public Matrix(int CurrentRow, int MaxRow, int StartCol, int MaxCol)
+        {
+            this.currentRow = CurrentRow;
+            this.maxRow = MaxRow;
+            this.startCol = StartCol;
+            this.maxCol = MaxCol;
+        }
+        /// <summary>
+        /// This Method builds the original Matrix
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="origArray"></param>
+        /// <returns></returns>
+        public int[,] BuildMatrix(string file, int[,] origArray)
+        {
+            currentRow = 0;
+            StreamReader reader = new StreamReader(file);
+            reader.ReadLine();
+            while (!reader.EndOfStream)
+            {
+                string lines = reader.ReadLine();
+
+                char[] delim = { ',' };
+                string[] fields = lines.Split(delim);
+                for (int col= startCol; col< maxCol; col++)
+                {
+                    origArray[currentRow, col] = int.Parse(fields[col]);
+                }
+                ++currentRow;             
+            }
+            return origArray;
+        }
+        /// <summary>
+        /// This method transposes the matrix
+        /// </summary>
+        /// <param name="origArray"></param>
+        /// <returns></returns>
+        public int[,] BuildTranspose(int[,] origArray)
+        {
+            currentRow = 0;
+            
+            for (int row = currentRow; row < maxRow; row++)
+            {
+                for (int col = startCol; col < maxCol; col++)
+                {
+                    transArray[col, row] = origArray[row, col];
+                }
+            }
+            return transArray;           
+        }
+
+        /// <summary>
+        /// This method calculates the trace value
+        /// </summary>
+        /// <param name="transArray"></param>
+        /// <returns></returns>
+        public int GetTrace(int[,] transArray)
+        {
+            for (int r = currentRow; r < maxRow; r++)
+            {
+                traceResult = traceResult + transArray[r,r];
+            }
+            return traceResult;
+        }
+                    
+       /// <summary>
+       /// This method returns the original empty array
+       /// </summary>
+       /// <returns></returns>
+     
+        public int[,] GetArray()
+        {
+            return twoDArray;
+        }
+    }
+}
+
+
+    
+
---
+++ 2 Dim Arrays/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+/// <summary>
+/// Billy Stockton
+/// 2DArray Exercise
+/// Due 6/10/2021
+/// This Program prints, transposes and traces matrices
+/// </summary>
+
+namespace Stockton2DArray
+{
+    class Program
+    {
+        /// <summary>
+        ///variables, constants and arrays
+        /// </summary>
+        public static int currentRowCt = 0;
+        public static int maxRowCt = 4;
+        public static int startColCt = 0;
+        public static int maxColCt = 4;
+        public static int[,] origMatrix;
+        public static int[,] transMatrix;
+        public static string FILE_NAME = "4x4matrix.txt";
+        
+        /// <summary>
+        /// Main Method 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Matrix matrix = new Matrix(currentRowCt,maxRowCt,startColCt,maxColCt);
+            origMatrix = matrix.BuildMatrix(FILE_NAME, matrix.GetArray());//Builds Original Array
+            transMatrix = matrix.BuildTranspose(origMatrix);//Transposes Array
+            PrintMatrix(origMatrix);//Prints Original Array
+            PrintTranspose(transMatrix);//Prints Transposed Array
+            CompareSymmetry(origMatrix,transMatrix);//Compares arrays for Symmetry
+            PrintTrace(matrix.GetTrace(transMatrix) ,transMatrix);//Prints Trace for Array
+        }
+
+        /// <summary>
+        /// This Method Prints the original matrix
+        /// </summary>
+        /// <param name="origMatrix"></param>
+        public static void PrintMatrix(int[,] origMatrix)
+        {
+            Console.WriteLine("\n4x4 Matrix\n");
+            int row, col;
+            for (row = currentRowCt; row < maxRowCt; ++row)
+            {
+                for (col = startColCt; col < maxColCt; ++col)                
+                    Console.Write(" {0}", origMatrix[row, col]);
+                    Console.WriteLine();               
+            }
+        }
+        /// <summary>
+        /// This Method Prints the Transposed Matrix
+        /// </summary>
+        /// <param name="transMatrix"></param>
+        public static void PrintTranspose(int[,] transMatrix)
+        {
+            Console.WriteLine("\nTransposed 4x4 Matrix\n");
+            int row, col;
+            for (row = currentRowCt; row < maxRowCt; ++row)
+            {
+                for (col = startColCt; col < maxColCt; ++col)               
+                    Console.Write(" {0}", transMatrix[row, col]);
+                    Console.WriteLine();                
+            }
+        }
+        /// <summary>
+        /// This Method compares to see if the Matrices are Symmetrical
+        /// </summary>
+        /// <param name="origMatrix"></param>
+        /// <param name="transMatrix"></param>
+        public static void CompareSymmetry(int[,] origMatrix, int[,] transMatrix)
+        {
+            if(origMatrix==transMatrix)
+            {
+                Console.WriteLine("\nMatrices are Symmetrical");
+            }
+            else
+            {
+                Console.WriteLine("\nMatrices are Not Symmetrical");
+            }
+        }
+        /// <summary>
+        /// This method Prints the trace and result of the Matrix
+        /// </summary>
+        /// <param name="traceResult"></param>
+        /// <param name="transMatrix"></param>
+        public static void PrintTrace(int traceResult, int[,] transMatrix)
+        {
+            Console.WriteLine("\nTrace Result\n");
+
+            for (int r = currentRowCt; r < maxRowCt; r++)
+            {
+                Console.Write("{0}", transMatrix[r, r]+"+");                
+            }
+            Console.Write("="+traceResult+"\n");
+        }
+    }   
+}
